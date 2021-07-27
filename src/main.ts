7fca8bcb45423f614cb4377cc8b7beac8f79da1e
NoehChan <noahkeithbrighton@hotmail.com>
2021-07-26 22:11:16 -0400

    changed owner field for github.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import * as route53 from '@aws-cdk/aws-route53';
 import * as targets from '@aws-cdk/aws-route53-targets';
 import * as s3deploy from '@aws-cdk/aws-s3-deployment';
-import { App, Construct, Stack, StackProps, Duration, Stage, StageProps, SecretValue } from '@aws-cdk/core';
+import {
+  App,
+  Construct,
+  Stack,
+  StackProps,
+  Duration,
+  Stage,
+  StageProps,
+  SecretValue,
+} from '@aws-cdk/core';
 import * as pipelines from '@aws-cdk/pipelines';
 import { CloudFrontToS3 } from '@aws-solutions-constructs/aws-cloudfront-s3';
 
@@ -21,10 +30,14 @@ export class Model3DotMoneyStack extends Stack {
 
     const domains = props.domains;
 
-    const publicZone = route53.HostedZone.fromHostedZoneAttributes(this, 'route53-zone', {
-      hostedZoneId: props.route53_HostedZoneId,
-      zoneName: props.route53_ZoneName,
-    });
+    const publicZone = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      'route53-zone',
+      {
+        hostedZoneId: props.route53_HostedZoneId,
+        zoneName: props.route53_ZoneName,
+      }
+    );
 
     const customCertificate = new acm.Certificate(this, 'custom-certificate', {
       domainName: domains[0],
@@ -46,13 +59,21 @@ export class Model3DotMoneyStack extends Stack {
 
       new route53.ARecord(this, `route53-a-record${counter}`, {
         zone: publicZone,
-        target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cloudfrontToS3Resource.cloudFrontWebDistribution)),
+        target: route53.RecordTarget.fromAlias(
+          new targets.CloudFrontTarget(
+            cloudfrontToS3Resource.cloudFrontWebDistribution
+          )
+        ),
         recordName: domain,
       });
 
       new route53.AaaaRecord(this, `route53-aaaa-record${counter}`, {
         zone: publicZone,
-        target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cloudfrontToS3Resource.cloudFrontWebDistribution)),
+        target: route53.RecordTarget.fromAlias(
+          new targets.CloudFrontTarget(
+            cloudfrontToS3Resource.cloudFrontWebDistribution
+          )
+        ),
         recordName: domain,
       });
     });
@@ -76,11 +97,16 @@ class DeployStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-
     new Model3DotMoneyStack(this, 'model3-money', {
-      route53_HostedZoneId: process.env.route53_HostedZoneId ?? this.node.tryGetContext('route53_HostedZoneId'),
-      route53_ZoneName: process.env.route53_ZoneName ?? this.node.tryGetContext('route53_ZoneName'),
-      domains: String(process.env.domains ?? this.node.tryGetContext('domains')).split(','),
+      route53_HostedZoneId:
+        process.env.route53_HostedZoneId ??
+        this.node.tryGetContext('route53_HostedZoneId'),
+      route53_ZoneName:
+        process.env.route53_ZoneName ??
+        this.node.tryGetContext('route53_ZoneName'),
+      domains: String(
+        process.env.domains ?? this.node.tryGetContext('domains')
+      ).split(','),
     });
   }
 }
@@ -96,7 +122,7 @@ export class PipelineStack extends Stack {
       actionName: 'GitHub',
       output: sourceArtifact,
       oauthToken: SecretValue.secretsManager('github-token'),
-      owner: 'aaronbrighton',
+      owner: 'NoehChan',
       repo: 'model3.money',
       branch: 'main',
     });
@@ -106,13 +132,19 @@ export class PipelineStack extends Stack {
       cloudAssemblyArtifact,
       environmentVariables: {
         route53_HostedZoneId: {
-          value: process.env.route53_HostedZoneId ?? this.node.tryGetContext('route53_HostedZoneId'),
+          value:
+            process.env.route53_HostedZoneId ??
+            this.node.tryGetContext('route53_HostedZoneId'),
         },
         route53_ZoneName: {
-          value: process.env.route53_ZoneName ?? this.node.tryGetContext('route53_ZoneName'),
+          value:
+            process.env.route53_ZoneName ??
+            this.node.tryGetContext('route53_ZoneName'),
         },
         domains: {
-          value: String(process.env.domains ?? this.node.tryGetContext('domains')).split(','),
+          value: String(
+            process.env.domains ?? this.node.tryGetContext('domains')
+          ).split(','),
         },
       },
       buildCommand: 'yarn build && yarn test',
@@ -128,9 +160,7 @@ export class PipelineStack extends Stack {
   }
 }
 
-
 const app = new App();
 new PipelineStack(app, 'model3-money-pipeline');
-
 
 app.synth();
